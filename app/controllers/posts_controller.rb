6b97126eba9d7a8bf72be30460168f5d4ce8fad1
Braylon Gurnell <braylon.gurnell@kellogg.northwestern.edu>
2022-06-07 22:59:03 +0000

    Post Grid

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,18 +3,22 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     @post.place_id = params["place_id"]
-    @post.user_id = session["user_id"]
+    @post.user_id = @current_user["user_id"]
   end
 
   def create
-    @post = Post.new
-    @post["title"] = params["post"]["title"]
-    @post["description"] = params["post"]["description"]
-    @post["posted_on"] = params["post"]["posted_on"]
-    @post.image.attach(params["post"]["image"])
-    @post["place_id"] = params["post"]["place_id"]
-    @post["user_id"] = session["user_id"]
-    @post.save
+    if @current_user
+      @post = Post.new
+      @post["title"] = params["post"]["title"]
+      @post["description"] = params["post"]["description"]
+      @post["posted_on"] = params["post"]["posted_on"]
+      @post.image.attach(params["post"]["image"])
+      @post["place_id"] = params["post"]["place_id"]
+      @post["user_id"] = @current_user["id"]
+      @post.save
+    else
+      flash["notice"] = "You must login first"
+    end
     redirect_to "/places/#{@post["place_id"]}"
   end
 
