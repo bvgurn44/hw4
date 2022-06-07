04e7d7a29df6cbca54548abbb55e712f313d4f87
Braylon Gurnell <braylon.gurnell@kellogg.northwestern.edu>
2022-06-07 20:32:48 +0000

    Authorization and authentication

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,7 +6,7 @@ class PlacesController < ApplicationController
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @posts = Post.where({ "place_id" => @place["id"] })
+    @posts = Post.where({ "place_id" => @place["id"], "user_id" => session["user_id"] })
   end
 
   def new
@@ -17,7 +17,7 @@ class PlacesController < ApplicationController
     @place = Place.new
     @place["name"] = params["place"]["name"]
     @place.save
-    redirect_to "/places"
+    redirect_to "/places/#{@place["id"]}"
   end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,7 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     @post.place_id = params["place_id"]
+    @post.user_id = @current_user["id"]
   end
 
   def create
@@ -10,7 +11,7 @@ class PostsController < ApplicationController
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
-    @post.uploaded_image.attach(params["post"]["uploaded_image"])
+    @post.image.attach(params["post"]["image"])
     @post["place_id"] = params["post"]["place_id"]
     @post["user_id"] = @current_user["id"]
     @post.save
